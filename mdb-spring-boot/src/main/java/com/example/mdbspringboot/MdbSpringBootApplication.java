aced2d88f8b2af5dbbd5c749af1578271d52fb99
suryamohan567@gmail.com <suryamohan567@gmail.com>
2023-04-07 14:09:02 +0530

    added parse in auto run

--- mdb-spring-boot/src/main/java/com/example/mdbspringboot/MdbSpringBootApplication.java
+++ mdb-spring-boot/src/main/java/com/example/mdbspringboot/MdbSpringBootApplication.java
@@ -3,6 +3,8 @@ package com.example.mdbspringboot;
 
 import java.io.IOException;
 
+import com.example.mdbspringboot.service.DataParseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +15,15 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 public class MdbSpringBootApplication implements CommandLineRunner{
 
+	@Autowired
+	DataParseService dataParseService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MdbSpringBootApplication.class, args);
 	}
 	
 	public void run(String... args) throws IOException {
-
+		dataParseService.startDataParsingFromHtmlToJavaPojo();
 	}
 }
 
--- mdb-spring-boot/src/main/java/com/example/mdbspringboot/dbEntity/UsedCarEntity.java
+++ mdb-spring-boot/src/main/java/com/example/mdbspringboot/dbEntity/UsedCarEntity.java
@@ -9,6 +9,44 @@ public class UsedCarEntity {
     @Id
     private String id;
 
+    private String partner;
 
+    private String partnerId;
+
+    private String variantName;
+
+    private String modelName;
+
+    private String makeName;
+
+    private String fullName;
+
+    private String carYear;
+
+    private String marketPrice;
+
+    private String mileage;
+
+    private String primaryFuel;
+
+    private String owner;
+
+    private String cityName;
+
+    private String emiStarts;
+
+    private String transmissionType;
+
+    private String color;
+
+    private String bodyType;
+
+    private String imageUrls;
+
+    private String webLink;
+
+    private String availabilityStatus;
+
+    private String detailImages;
 
 }
